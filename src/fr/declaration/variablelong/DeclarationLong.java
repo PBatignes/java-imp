46b2064129b3d84be288ea0aaf0de06ad829697b
Pierre BATIGNES <batignesp@gmail.com>
2020-02-28 13:20:59 +0100

    Commit Long

---
+++ src/fr/declaration/variablelong/DeclarationLong.java
@@ -0,0 +1,16 @@
+package fr.declaration.variablelong;
+
+public class DeclarationLong {
+	
+	public static void main (String[] args ) {
+		
+		long a = 3000L;
+		long b = 3000;
+		// 3000 est un int 3000L est un long
+		long c = 3000000000L;
+		// En enlevant le L java transforme le nombre en int, or 3000000000 est trop grand pour un int
+		
+		//L et l sont pareils
+	}
+
+}
